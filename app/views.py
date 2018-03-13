e7880af5fa49c01963f4abd2c1868d733cc630be
kashkama <owen.kamau2014@gmail.com>
2018-03-13 20:12:28 +0300

    adding weather api

--- app/views.py
+++ app/views.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask import render_template
 import feedparser
 from flask import request
+import json
+import urllib.request as urllib2
+import urllib.parse
 
 app = Flask(__name__)
 
@@ -11,16 +14,48 @@ RSS_FEEDS = {
     "fox": "http://feeds.foxnews.com/foxnews/latest",
     "iol": "http://www.iol.co.za/cmlink/1.640"
 }
+DEFAULTS = {"publication": "bbc", "city":"Nairobi,Kenya"}
 
 @app.route("/")
-def get_news():
-    query = request.args.get("publication")
+def home():
+    # get customized headlines, based on user input or default
+    publication = request.args.get("publication")
+    if not publication:
+        publication = DEFAULTS["publication"]
+
+    articles = get_news(publication)
+    # get customized weather based on user input or default
+    city = request.args.get("city")
+    if not city:
+        city = DEFAULTS["city"]
+    
+    weather = get_weather(city)
+
+    return render_template("home.html", articles=articles, weather=weather)
+
+def get_news(query):
     if not query or query.lower() not in RSS_FEEDS:
-        publication = "bbc"
+        publication = DEFAULTS["publication"]
     else:
         publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    return render_template("home.html", articles = feed["entries"])
+    return feed["entries"]
+
+def get_weather(query):
+    api_url =  "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=2d5b161aa35a6b40ab5b8f3fc1f7d8a3"
+    query = urllib.parse.quote(query)
+    url = api_url.format(query)
+    data = urllib2.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get("weather"):
+        weather = {
+            "description": parsed["weather"][0]["description"],
+            "temperature": parsed["main"]["temp"],
+            "city": parsed["name"]
+        }
+    return weather
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
